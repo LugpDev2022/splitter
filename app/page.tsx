271d56049db5beb347535b394216e4ad32979e76
LugpDev2022 <lugpdev2022@gmail.com>
2023-04-06 12:14:50 -0600

    Ammount span

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Input from './components/Input';
 import Title from './components/Title';
+import Button from './components/buttons/Button';
 import SectionSpan from './components/spans/SectionSpan';
+
 import dollarIcon from './assets/icon-dollar.svg';
 import personIcon from './assets/icon-person.svg';
-import Button from './components/buttons/Button';
+import AmmountSpan from './components/spans/AmmountSpan';
 
 const percentages: number[] = [5, 10, 15, 25, 50];
 
@@ -15,34 +17,55 @@ const page = () => {
       flex-col
       gap-8
       justify-end
-      md:justify-center'
+      md:justify-center
+      '
       style={{ height: '100%', alignItems: 'center' }}
     >
       <header className='flex flex-col justify-center'>
         <Title />
       </header>
 
-      <div className=' bg-white rounded-t-[35px] md:rounded-[30px] p-7 w-full md:w-11/12 lg:w-9/12'>
+      <div
+        className=' 
+      bg-white
+      gap-7
+      grid
+      grid-cols-1
+      lg:w-9/12
+      md:grid-cols-2
+      md:rounded-[30px]
+      md:w-11/12
+      p-7
+      rounded-t-[35px]
+      w-full
+      '
+      >
         <div>
-          <SectionSpan>Bill</SectionSpan>
-          <Input placeholder='0' icon={dollarIcon} iconAlt='dollar icon' />
-        </div>
-        <div className='mt-4'>
-          <SectionSpan>Select Tip %</SectionSpan>
-          <div className='grid grid-cols-2 md:grid-cols-3 gap-4'>
-            {percentages.map((percentage: number) => (
-              <Button key={percentage}>{percentage}</Button>
-            ))}
+          <div>
+            <SectionSpan>Bill</SectionSpan>
+            <Input placeholder='0' icon={dollarIcon} iconAlt='dollar icon' />
+          </div>
+          <div className='mt-4'>
+            <SectionSpan>Select Tip %</SectionSpan>
+            <div className='grid grid-cols-2 md:grid-cols-3 gap-4'>
+              {percentages.map((percentage: number) => (
+                <Button key={percentage}>{percentage}</Button>
+              ))}
+            </div>
+          </div>
+          <div className='mt-4'>
+            <SectionSpan>Number of People</SectionSpan>
+            <Input
+              placeholder='0'
+              icon={personIcon}
+              iconWidth={20}
+              iconAlt='person icon'
+            />
           </div>
         </div>
-        <div className='mt-4'>
-          <SectionSpan>Number of People</SectionSpan>
-          <Input
-            placeholder='0'
-            icon={personIcon}
-            iconWidth={20}
-            iconAlt='person icon'
-          />
+        <div className='bg-[#00494d] rounded-xl p-7'>
+          <AmmountSpan ammount={0.0}>Tip Ammmount</AmmountSpan>
+          <AmmountSpan ammount={0.0}>Total</AmmountSpan>
         </div>
       </div>
     </div>
