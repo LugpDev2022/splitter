c9974352a6fd789d1ee6bf2638fb1e99697edac0
LugpDev2022 <lugpdev2022@gmail.com>
2023-04-06 07:15:20 -0600

    Make the page division

--- app/components/Title.tsx
+++ app/components/Title.tsx
@@ -1,14 +1,12 @@
 const Title = () => (
-  <div className='h-1/5 flex flex-col justify-center'>
-    <h1
-      className='text-3xl tracking-[.3em] font-semibold text-center'
-      style={{ color: 'hsl(183, 100%, 15%)' }}
-    >
-      SPLI
-      <br />
-      TTER
-    </h1>
-  </div>
+  <h1
+    className='text-3xl tracking-[.3em] font-semibold text-center'
+    style={{ color: 'hsl(183, 100%, 15%)' }}
+  >
+    SPLI
+    <br />
+    TTER
+  </h1>
 );
 
 export default Title;
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,11 @@ import Title from './components/Title';
 const page = () => {
   return (
     <>
-      <Title />
+      <header className='h-1/5 flex flex-col justify-center'>
+        <Title />
+      </header>
+
+      <div className='h-full bg-white rounded-t-[35px] p-6'></div>
     </>
   );
 };
