f1f30f772b6773f9e2517bdf939693cabc17cc16
LugpDev2022 <lugpdev2022@gmail.com>
2023-04-05 18:30:16 -0600

    Import space mono font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import './globals.css';
+import { Space_Mono } from 'next/font/google';
 
 export const metadata = {
   title: 'Splitter | Luis Cervantes',
 };
+
+const spaceMono = Space_Mono({ weight: ['400', '700'], subsets: ['latin'] });
 
 export default function RootLayout({
   children,
@@ -14,7 +17,7 @@ export default function RootLayout({
       <head>
         <link rel='shortcut icon' href='/favicon.png' />
       </head>
-      <body>{children}</body>
+      <body className={spaceMono.className}>{children}</body>
     </html>
   );
 }
