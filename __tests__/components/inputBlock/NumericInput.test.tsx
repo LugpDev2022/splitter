a9a4cecd1f5367e0c2f9b5986d8209abdf04be75
LugpDev2022 <lugpdev2022@gmail.com>
2023-04-10 08:18:56 -0600

    Update NumericInput tests

--- __tests__/components/inputBlock/NumericInput.test.tsx
+++ __tests__/components/inputBlock/NumericInput.test.tsx
@@ -1,26 +1,28 @@
-import NumericInput from '@/app/components/inputBlock/NumericInput';
 import { fireEvent, render, screen } from '@testing-library/react';
+import NumericInput from '@/app/components/inputBlock/NumericInput';
+import { useNumericInput } from '../../../app/components/inputBlock/useNumericInput';
+
+jest.mock('../../../app/components/inputBlock/useNumericInput');
 
 describe('tests on <NumericInput />', () => {
-  const onChange = jest.fn();
+  const onInputChange = jest.fn();
   const onBlur = jest.fn();
 
-  beforeEach(() => {
-    jest.clearAllMocks();
+  useNumericInput.mockReturnValue({
+    onInputChange,
+    onBlur,
   });
 
+  beforeEach(() => jest.clearAllMocks());
+
   test('should match snapshot', () => {
-    const { container } = render(
-      <NumericInput onBlur={onBlur} onChange={onChange} value={0} />
-    );
+    const { container } = render(<NumericInput inputType='bill' value={0} />);
 
     expect(container).toMatchSnapshot();
   });
 
   test('should have a border when has error', () => {
-    render(
-      <NumericInput onBlur={onBlur} onChange={onChange} hasError value={0} />
-    );
+    render(<NumericInput inputType='bill' hasError value={0} />);
 
     const input = screen.getByRole('textbox');
     expect(input.getAttribute('class')).toContain('border-red-500');
@@ -28,7 +30,7 @@ describe('tests on <NumericInput />', () => {
   });
 
   test('should not have a border when has error', () => {
-    render(<NumericInput onBlur={onBlur} onChange={onChange} value={0} />);
+    render(<NumericInput inputType='bill' value={0} />);
 
     const input = screen.getByRole('textbox');
     expect(input.getAttribute('class')).not.toContain('border-red-500');
@@ -36,20 +38,27 @@ describe('tests on <NumericInput />', () => {
   });
 
   test('should call the onChange function', () => {
-    render(<NumericInput onBlur={onBlur} onChange={onChange} value={0} />);
+    render(<NumericInput inputType='bill' value={0} />);
 
     const input = screen.getByRole('textbox');
 
     fireEvent.change(input, { target: { value: '7' } });
-    expect(onChange).toHaveBeenCalledTimes(1);
+    expect(onInputChange).toHaveBeenCalledTimes(1);
   });
 
   test('should call the onBlur function', () => {
-    render(<NumericInput onBlur={onBlur} onChange={onChange} value={0} />);
+    render(<NumericInput inputType='bill' value={0} />);
 
     const input = screen.getByRole('textbox');
 
     fireEvent.blur(input);
     expect(onBlur).toHaveBeenCalledTimes(1);
+  });
+
+  test('custom hook should be called with the input type value', () => {
+    const inputType = 'bill';
+    render(<NumericInput inputType={inputType} value={0} />);
+
+    expect(useNumericInput).toHaveBeenCalledWith(inputType);
   });
 });
